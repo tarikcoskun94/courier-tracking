06e9ae2d017303f685115be87e70c4678b759690
Tarık COŞKUN <tarikcoskun@hotmail.com.tr>
2025-02-01 01:40:44 +0300

    Initialize Spring Boot project with Maven configuration.
    Set up the base structure for the Courier Tracking application using Spring Boot. Added Maven dependencies, application properties, and .gitignore for IDE and build tool exclusions. Included a simple test case and Maven Wrapper for consistent builds.

---
+++ src/test/java/com/migrosone/couriertracking/CourierTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migrosone.couriertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
